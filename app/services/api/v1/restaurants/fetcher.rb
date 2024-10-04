98a0aedb0132ba152651f4c95e2905ef12b86a1d
Luisfelipedev27 <luisfelipe1582@hotmail.com>
2024-10-04 20:08:51 -0300

    organize object json restaurants

--- app/services/api/v1/restaurants/fetcher.rb
+++ app/services/api/v1/restaurants/fetcher.rb
@@ -15,9 +15,22 @@ module Api
         attr_writer :restaurants
 
         def fetch_restaurants
-          self.restaurants = Restaurant.all
-
-          true
+          self.restaurants = Restaurant.all.map do |restaurant|
+            {
+              name: restaurant.name,
+              menus: restaurant.menus.map do |menu|
+                {
+                  name: menu.name,
+                  menu_items: menu.menu_items.map do |menu_item|
+                    {
+                      name: menu_item.name,
+                      price: menu_item.price
+                    }
+                  end
+                }
+              end
+            }
+          end
         end
       end
     end
