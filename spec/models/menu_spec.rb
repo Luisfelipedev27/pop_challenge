cb28da79d46f34d224d2647b8639d3f31486c729
Luisfelipedev27 <luisfelipe1582@hotmail.com>
2024-10-04 17:08:23 -0300

    add test to menu model

--- spec/models/menu_spec.rb
+++ spec/models/menu_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Menu, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:name) }
+  end
+
+  describe 'associations' do
+    it { is_expected.to belong_to(:restaurant) }
+    it { is_expected.to have_many(:menu_items) }
+  end
 end
