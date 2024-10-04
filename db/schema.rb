55f38f53c422a876e5a35e085651b230ee0f20b0
Luisfelipedev27 <luisfelipe1582@hotmail.com>
2024-10-04 16:46:54 -0300

    create table menu_items

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_10_04_194254) do
+ActiveRecord::Schema.define(version: 2024_10_04_194412) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "menu_items", force: :cascade do |t|
+    t.string "name"
+    t.decimal "price"
+    t.bigint "menu_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["menu_id"], name: "index_menu_items_on_menu_id"
+  end
 
   create_table "menus", force: :cascade do |t|
     t.string "name"
@@ -29,5 +38,6 @@ ActiveRecord::Schema.define(version: 2024_10_04_194254) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "menu_items", "menus"
   add_foreign_key "menus", "restaurants"
 end
