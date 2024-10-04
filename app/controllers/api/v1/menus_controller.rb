f6628f3538c2c17e698e7e95a4ed2adb5aa45d6c
Luisfelipedev27 <luisfelipe1582@hotmail.com>
2024-10-04 17:44:17 -0300

    adjust success and not success scenario in menus controller

--- app/controllers/api/v1/menus_controller.rb
+++ app/controllers/api/v1/menus_controller.rb
@@ -4,7 +4,21 @@ module Api
       def index
         service = Api::V1::Menus::Fetcher.call
 
-        render json: service.menus
+        if service.success?
+          render json: service.menus
+        else
+          render json: { error: service.error_message }, status: :not_found
+        end
+      end
+
+      def show
+        service = Api::V1::Menus::Renderer.call(menu_id: params[:id])
+
+        if service.success?
+          render json: service.menu
+        else
+          render json: { error: service.error_message }, status: :not_found
+        end
       end
     end
   end
