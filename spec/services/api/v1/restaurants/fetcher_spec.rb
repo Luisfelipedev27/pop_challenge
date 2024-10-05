5732f3acd27bcf790649ecd2b4c99106716ac9ab
Luisfelipedev27 <luisfelipe1582@hotmail.com>
2024-10-05 00:32:57 -0300

    add restaurant response to test scenario

--- spec/services/api/v1/restaurants/fetcher_spec.rb
+++ spec/services/api/v1/restaurants/fetcher_spec.rb
@@ -6,16 +6,34 @@ RSpec.describe Api::V1::Restaurants::Fetcher do
       it 'returns all restaurants' do
         restaurant = create(:restaurant)
 
+        restaurant_response = {
+          name: restaurant.name,
+          menus: restaurant.menus.map do |menu|
+            {
+              name: menu.name,
+              menu_items: menu.menu_items.map do |menu_item|
+                {
+                  name: menu_item.name,
+                  price: menu_item.price
+                }
+              end
+            }
+          end
+        }
+
         service = described_class.call
 
-        expect(service.restaurants).to eq([restaurant])
+        expect(service).to be_success
+        expect(service.restaurants).to eq([restaurant_response])
       end
     end
 
     context 'when you can not find restaurants' do
       it 'returns an empty array' do
+
         service = described_class.call
 
+        expect(service).to be_success
         expect(service.restaurants).to eq([])
       end
     end
